3279e639cc49f6527ce4d0e771e6dea183e1b968
Clair Blacketer <clair.blacketer@gmail.com>
2021-03-12 09:20:08 -0500

    Adds SQL to find COVID-19 vaccines in OPTUM EHR and adds new source vocabulary to map vaccines.

---
+++ docs/OPTUM_PANTHER/Vocab Updates/Optum_EHR_Covid_Vaccines.sql
@@ -0,0 +1,10 @@
+--Query to get covid vaccines from Optum EHR COVID
+
+select count(*), immunization_desc, ndc, ndc_source
+from native.immunizations
+where immunization_desc ilike ('%pfizer%') or
+      immunization_desc ilike ('%covid%') or
+      immunization_desc ilike ('%moderna%') or 
+      immunization_desc ilike ('%astra%') or
+      immunization_desc ilike ('%janssen%')
+group by immunization_desc, ndc, ndc_source
