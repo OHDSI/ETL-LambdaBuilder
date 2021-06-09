4f204a633113344e5e9691913c5e0266b6459199
Andryc <AAndryc@its.jnj.com>
2021-06-09 09:12:24 -0400

    Updated test cases in collaboration with Clair Blacketer

--- docs/IBM_CCAE_MDCR/Test Cases/R/ProviderTests.R
+++ docs/IBM_CCAE_MDCR/Test Cases/R/ProviderTests.R
@@ -7,6 +7,12 @@ createProviderTests <- function () {
     patient<-createPatient();
     add_facility_header(enrolid=patient$enrolid, provid=provider$provid, stdprov="220")
     expect_provider(specialty_concept_id="38004510", specialty_source_value="220")
+    
+    provider<-createProvider();
+    declareTest(id = provider$provid, description = "Provider in facility header table with stdprov=''. Id is PROVIDER_SOURCE_VALUE")
+    patient<-createPatient();
+    add_facility_header(enrolid=patient$enrolid, provid=provider$provid, stdprov="")
+    expect_provider(specialty_concept_id="38004514")
   
     provider<-createProvider();
     declareTest(id = provider$provid, description = "Provider in outpatient_services table with stdprov=285, specialty = infectious disease. Id is PROVIDER_SOURCE_VALUE")
@@ -30,7 +36,7 @@ createProviderTests <- function () {
     declareTest(id = provider$provid, description = "Even when PROVID IS NULL create a PROVIDER (HIX-1404). Id is PROVIDER_SOURCE_VALUE")
     patient<-createPatient();
     add_facility_header(enrolid=patient$enrolid, provid=NULL, stdprov="220")
-    expect_provider( provider_source_value = '', specialty_source_value="220")
+    expect_provider(provider_source_value =NULL, specialty_source_value="220")
   }
   
   if (Sys.getenv("truvenType") == "MDCD")
