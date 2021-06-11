72a6e9161dc2aecb06de6fd1bdbdcf3f6d1435d5
Andryc <AAndryc@its.jnj.com>
2021-06-11 14:15:05 -0400

    procedure and measurment test case updates

--- docs/IBM_CCAE_MDCR/Test Cases/R/MeasurementTests.R
+++ docs/IBM_CCAE_MDCR/Test Cases/R/MeasurementTests.R
@@ -3,7 +3,7 @@ createMeasurementTests <- function () {
   
   if (Sys.getenv("truvenType") != "MDCD")
   {
-    
+  
   patient <- createPatient()
   encounter <- createEncounter()
   declareTest(id = patient$person_id, "LOINC code does not have a dash in the second to last character, record is dropped. Id is PERSON_ID.")
@@ -16,7 +16,8 @@ createMeasurementTests <- function () {
   declareTest(id = patient$person_id, "Svcdate is outside of observation period, record is dropped. Id is PERSON_ID.")
   add_enrollment_detail(enrolid=patient$enrolid, dtend = '2012-12-31', dtstart = '2012-01-01')
   add_lab(enrolid = patient$enrolid, year='2012', svcdate = '2013-07-09', loinccd = '56773-5')
-  expect_no_measurement(person_id = patient$person_id)
+  expect_measurement(person_id = patient$person_id)
+  
   
   patient <- createPatient()
   encounter <- createEncounter()
@@ -27,10 +28,40 @@ createMeasurementTests <- function () {
   
   patient <- createPatient()
   encounter <- createEncounter()
+  declareTest(id = patient$person_id, "Patient has abnormal=H; value as concept_id = 4267416. Id is PERSON_ID.")
+  add_enrollment_detail(enrolid=patient$enrolid, dtend = '2012-12-31', dtstart = '2012-01-01')
+  add_lab(enrolid = patient$enrolid, year='2012', svcdate = '2012-12-20', loinccd = '56784-2', abnormal = 'H')
+  expect_measurement(person_id = patient$person_id, value_as_concept_id = '4328749')
+  
+  patient <- createPatient()
+  encounter <- createEncounter()
+  declareTest(id = patient$person_id, "Patient has abnormal=N; value as concept_id = 4267416. Id is PERSON_ID.")
+  add_enrollment_detail(enrolid=patient$enrolid, dtend = '2012-12-31', dtstart = '2012-01-01')
+  add_lab(enrolid = patient$enrolid, year='2012', svcdate = '2012-12-20', loinccd = '56784-2', abnormal = 'N')
+  expect_measurement(person_id = patient$person_id, value_as_concept_id = '4069590')
+  
+  patient <- createPatient()
+  encounter <- createEncounter()
+  declareTest(id = patient$person_id, "Patient has abnormal=A; value as concept_id = 4267416. Id is PERSON_ID.")
+  add_enrollment_detail(enrolid=patient$enrolid, dtend = '2012-12-31', dtstart = '2012-01-01')
+  add_lab(enrolid = patient$enrolid, year='2012', svcdate = '2012-12-20', loinccd = '56784-2', abnormal = 'A')
+  expect_measurement(person_id = patient$person_id, value_as_concept_id = '4135493')
+  
+  patient <- createPatient()
+  encounter <- createEncounter()
+  declareTest(id = patient$person_id, "Patient has abnormal=+; value as concept_id = 4267416. Id is PERSON_ID.")
+  add_enrollment_detail(enrolid=patient$enrolid, dtend = '2012-12-31', dtstart = '2012-01-01')
+  add_lab(enrolid = patient$enrolid, year='2012', svcdate = '2012-12-20', loinccd = '56784-2', abnormal = '+')
+  expect_measurement(person_id = patient$person_id, value_as_concept_id = '9191')
+  
+  
+  patient <- createPatient()
+  encounter <- createEncounter()
   declareTest(id = patient$person_id, "Patient has result ne 0 and > neg 99999, result listed in value as number field. Id is PERSON_ID.")
   add_enrollment_detail(enrolid=patient$enrolid, dtend = '2012-12-31', dtstart = '2012-01-01')
   add_lab(enrolid = patient$enrolid, year='2012', svcdate = '2012-08-24', loinccd = '56789-1', result = '120')
   expect_measurement(person_id = patient$person_id, value_as_number = '120')
+  
   
   patient <- createPatient()
   encounter <- createEncounter()
@@ -52,6 +83,27 @@ createMeasurementTests <- function () {
   add_enrollment_detail(enrolid=patient$enrolid, dtend = '2012-12-31', dtstart = '2012-01-01')
   add_lab(enrolid = patient$enrolid, year='2012', svcdate = '2012-04-06', loinccd = '29463-7', result = '1500000', resunit = 'lbs.')
   expect_measurement(person_id = patient$person_id, value_as_number = '150', unit_concept_id = '8739')
+  
+  patient <- createPatient()
+  encounter <- createEncounter()
+  declareTest(id = patient$person_id, "Patient has REFHIGH and REFLOW mapping correctly. Check to ensure floats convert correctly. Id is PERSON_ID.")
+  add_enrollment_detail(enrolid=patient$enrolid, dtend = '2012-12-31', dtstart = '2012-01-01')
+  add_lab(enrolid = patient$enrolid, year='2012', svcdate = '2012-12-20', loinccd = '56784-2', refhigh = 10.8, reflow = 1)
+  expect_measurement(person_id = patient$person_id, value_as_concept_id = '9191', range_high = 10.8, range_low = 1)
+  
+  patient <- createPatient()
+  encounter <- createEncounter()
+  declareTest(id = patient$person_id, "Patient has a LOINC of 3142-7, 29463-7, 3141-9 and RESULT > 100000 and RESUNIT = 'LBS'")
+  add_enrollment_detail(enrolid=patient$enrolid, dtend = '2012-12-31', dtstart = '2012-01-01')
+  add_lab(enrolid = patient$enrolid, year='2012', svcdate = '2012-04-06', loinccd = '29463-7', result = '1950000', resunit = 'LBS')
+  expect_measurement(person_id = patient$person_id, value_as_number = '195', unit_concept_id = '8739')
+  
+  patient <- createPatient()
+  encounter <- createEncounter()
+  declareTest(id = patient$person_id, "Patient has a LOINC of 3142-7, 29463-7, 3141-9 and RESULT > 100000 and RESUNIT = 'LBS'.  No dropping of zeros")
+  add_enrollment_detail(enrolid=patient$enrolid, dtend = '2012-12-31', dtstart = '2012-01-01')
+  add_lab(enrolid = patient$enrolid, year='2012', svcdate = '2012-04-06', loinccd = '29463-7', result = '195000', resunit = 'LBS')
+  expect_measurement(person_id = patient$person_id, value_as_number = '195000', unit_concept_id = '8739')
   
   }
   
