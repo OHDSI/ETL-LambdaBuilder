9ee44cc41be3894cf14a32592eda6cf03396a868
Anton Ivanov <antoha.ivanov@gmail.com>
2023-11-16 18:15:28 +0300

    Update ProcedureOccurrenceTests.R

--- docs/IBM_CCAE_MDCR/Test Cases/R/ProcedureOccurrenceTests.R
+++ docs/IBM_CCAE_MDCR/Test Cases/R/ProcedureOccurrenceTests.R
@@ -36,14 +36,6 @@ createProcedureOccurrenceTests <- function () {
   add_inpatient_services(caseid = encounter$caseid, enrolid = patient$enrolid, pproc = '65779', svcdate = '2022-05-15', tsvcdat = '2022-05-17', qty = NULL, year = '2022')
   expect_procedure_occurrence(person_id = patient$person_id, procedure_date = '2022-05-15', quantity = NULL)
   
-  patient <- createPatient()
-  encounter <- createEncounter()
-  declareTest(id = patient$person_id, "Move all records in the PROCEDURE_OCCURRENCE table where the PROCEDURE_SOURCE_VALUE = 'S0013' to the DRUG_EXPOSURE table with the DRUG_CONCEPT_ID = 37496953. Id is PERSON_ID")
-  add_enrollment_detail(enrolid=patient$enrolid, dtend = '2012-12-31', dtstart = '2012-01-01')
-  add_inpatient_services(caseid = encounter$caseid, enrolid = patient$enrolid, pproc = 'S0013', svcdate = '2022-05-15', tsvcdat = '2022-05-17', year = '2022')
-  expect_drug_exposure(person_id = patient$person_id, drug_concept_id = '37496953', drug_exposure_start_date = '2022-05-15')
-  
-  
   if (truvenType != "MDCD") {
     patient <- createPatient()
     encounter <- createEncounter()
