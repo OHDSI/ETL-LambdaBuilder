2ce412e5d61349bfb96726d45f8f361c313fb92c
Anton Ivanov <antoha.ivanov@gmail.com>
2024-02-19 17:01:23 +0300

    Revert "fix JMDC unit tests"

--- docs/JMDC/Test Cases/R/CareSite.R
+++
@@ -1,6 +0,0 @@
-# Care site ------------------------------------------------------------------
-createCareSiteTests <- function () {
-	declareTest(301, "Care site id")
-	add_medical_facility(medical_facility_id = "F0000001")
-	expect_care_site(care_site_id = 10000001, care_site_source_value = "F0000001")
-}
