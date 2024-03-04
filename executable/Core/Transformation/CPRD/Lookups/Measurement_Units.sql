fc3d68384708fa0c30769f92c71f8b96a2ada17d
Anton Ivanov <antoha.ivanov@gmail.com>
2024-03-04 15:01:02 +0300

    executable cleaned up

--- executable/Core/Transformation/CPRD/Lookups/Measurement_Units.sql
+++
@@ -1,3 +0,0 @@
-﻿SELECT distinct concept_name, concept_id, 'None' as Domain, NULL AS valid_start_date, NULL AS valid_end_date
-FROM {sc}.concept
-where lower(domain_id)='unit' and lower(vocabulary_id)='ucum'
