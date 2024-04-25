8c83ff5b9900f0572a0449091723030c93e4c26c
Anton Ivanov <antoha.ivanov@gmail.com>
2024-04-25 23:05:07 +0300

    Update Batch.sql

--- sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/CDM/Batch.sql
+++ sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/CDM/Batch.sql
@@ -3,10 +3,6 @@
 SELECT DISTINCT PERSON_ID, PERSON_ID
 FROM {sc}.PERSON
 where PERSON_ID in (SELECT distinct PERSON_ID FROM {sc}.drug_exposure where drug_source_value = 'J9380')
-union
-SELECT DISTINCT PERSON_ID, PERSON_ID
-FROM {sc}.PERSON
-where PERSON_ID not in (SELECT distinct PERSON_ID FROM {sc}.drug_exposure where drug_source_value = 'J9380')
 ) a
 order by 1
 limit 1000000;
