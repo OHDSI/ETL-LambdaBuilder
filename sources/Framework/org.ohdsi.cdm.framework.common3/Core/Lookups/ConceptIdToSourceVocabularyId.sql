33ea61fbadcc24534ab223b19233e0b1ece1d393
Anton Ivanov <antoha.ivanov@gmail.com>
2024-05-02 12:16:06 +0300

    Update ConceptIdToSourceVocabularyId.sql

--- sources/Framework/org.ohdsi.cdm.framework.common3/Core/Lookups/ConceptIdToSourceVocabularyId.sql
+++ sources/Framework/org.ohdsi.cdm.framework.common3/Core/Lookups/ConceptIdToSourceVocabularyId.sql
@@ -1,16 +1,3 @@
 ﻿SELECT distinct concept_id, vocabulary_id, domain_id
 FROM {sc}.concept
-where domain_id in ('Condition', 'Device', 'Drug', 'Measurement', 'Observation', 'Procedure')
---where vocabulary_id in
---(
---'CPT4',
---'HCPCS',
---'ICD10CM',
---'ICD10PCS',
---'ICD10',
---'LOINC',
---'ICD9CM',
---'ICD9Proc',
---'NDC',
---'Revenue Code'
---)
+where domain_id in ('Condition', 'Device', 'Drug', 'Measurement', 'Observation', 'Procedure');
