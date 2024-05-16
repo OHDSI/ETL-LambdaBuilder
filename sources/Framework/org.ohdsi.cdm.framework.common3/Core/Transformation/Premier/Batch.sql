9299456665e0fd28ced582a04891c2ddcf3369fb
Anton Ivanov <antoha.ivanov@gmail.com>
2024-05-16 17:49:03 +0300

    Update Batch.sql

--- sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/Premier/Batch.sql
+++ sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/Premier/Batch.sql
@@ -1,8 +1,4 @@
-﻿SELECT DISTINCT {0} medrec_key, medrec_key
-FROM {sc}.pat
-WHERE medrec_key is not NULL and medrec_key in
-(
-SELECT DISTINCT cast(pat.medrec_key as bigint) as medrec_key
+﻿SELECT DISTINCT medrec_key, medrec_key
 FROM {sc}.pat
 JOIN {sc}.patcpt cpt ON PAT.PAT_KEY = cpt.pat_key
 where cpt.cpt_code in
@@ -50,7 +46,6 @@ where cpt.cpt_code in
 'Q2040',
 'Q2041',
 'Q2042'
-)
 )
 order by 1
 limit 1000000
