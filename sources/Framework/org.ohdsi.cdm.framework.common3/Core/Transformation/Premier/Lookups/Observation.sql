aa296d6f462320a0fc02009093713e5ec7aa4919
Anton Ivanov <antoha.ivanov@gmail.com>
2024-05-16 11:46:46 +0300

    Update Observation.sql

--- sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/Premier/Lookups/Observation.sql
+++ sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/Premier/Lookups/Observation.sql
@@ -5,7 +5,7 @@ FROM Source_to_Standard
 WHERE lower(SOURCE_VOCABULARY_ID) IN ('cpt4', 'drg', 'hcpcs', 'jnj_pmr_obs_code', 'jnj_pmr_proc_chrg_cd')
 AND lower(TARGET_DOMAIN_ID) = 'observation'
 ), Source as (
-SELECT distinct SOURCE_CODE, TARGET_CONCEPT_ID, SOURCE_VALID_START_DATE, SOURCE_VALID_END_DATE
+SELECT distinct SOURCE_CODE, TARGET_CONCEPT_ID, SOURCE_VALID_START_DATE, SOURCE_VALID_END_DATE, null as SOURCE_INVALID_REASON
 FROM Source_to_Source
 WHERE lower(SOURCE_VOCABULARY_ID) IN ('icd9cm', 'cpt4', 'hcpcs', 'mdc')
 AND lower(TARGET_VOCABULARY_ID) IN  ('icd9cm', 'cpt4', 'hcpcs', 'mdc') and lower(TARGET_DOMAIN_ID)='observation'
@@ -26,6 +26,3 @@ from S_S
 left join Standard on Standard.SOURCE_CODE = S_S.SOURCE_CODE
 left join Source on Source.SOURCE_CODE = S_S.SOURCE_CODE 
 left join ingredient_level on ingredient_level.concept_id = Standard.TARGET_CONCEPT_ID
-
-
-
