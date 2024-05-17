b9e671f1e7bd4bed8a4334f89e848c036e83cdb8
Anton Ivanov <antoha.ivanov@gmail.com>
2024-05-17 10:47:22 +0300

    fix

--- sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/Premier/Batch.sql
+++ sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/Premier/Batch.sql
@@ -1,25 +1,6 @@
 ﻿SELECT DISTINCT medrec_key, medrec_key
 FROM {sc}.pat
 JOIN {sc}.patcpt cpt ON PAT.PAT_KEY = cpt.pat_key
-where cpt.cpt_code in
-(
-'G0050',
-'G0308',
-'G0309',
-'G0310',
-'G0311',
-'G0312',
-'G0313',
-'G0314',
-'G0315',
-'G0316',
-'G0317',
-'G0318',
-'G0320',
-'G0321',
-'G0322',
-'G0323',
-'G0327'
-)
+where cpt.cpt_code in ('J1440', 'J9350', 'J9380', 'Q2040', 'Q2041', 'Q2042')
 order by 1
 limit 1000000
--- sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/Premier/Lookups/DrugCpt.sql
+++ sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/Premier/Lookups/DrugCpt.sql
@@ -4,8 +4,20 @@ SELECT distinct SOURCE_CODE, TARGET_CONCEPT_ID, TARGET_DOMAIN_ID, SOURCE_VALID_S
 FROM Source_to_Standard
 WHERE lower(SOURCE_VOCABULARY_ID) IN ('cpt4', 'hcpcs')
 AND lower(TARGET_DOMAIN_ID) = 'drug'
+), Source as (
+SELECT distinct SOURCE_CODE, TARGET_CONCEPT_ID, SOURCE_VALID_START_DATE, SOURCE_VALID_END_DATE, SOURCE_INVALID_REASON
+FROM Source_to_Source
+WHERE lower(SOURCE_VOCABULARY_ID) IN ('cpt4', 'hcpcs')
+AND lower(TARGET_VOCABULARY_ID) IN ('cpt4', 'hcpcs') AND lower(TARGET_DOMAIN_ID)='drug'
+), S_S as
+(
+select SOURCE_CODE from Standard
+union 
+select SOURCE_CODE from Source
 )
 
-select distinct Standard.SOURCE_CODE, Standard.TARGET_CONCEPT_ID, Standard.TARGET_DOMAIN_ID, Standard.VALID_START_DATE, Standard.VALID_END_DATE, Standard.SOURCE_VOCABULARY_ID, NULL as SOURCE_TARGET_CONCEPT_ID, NULL as SOURCE_VALID_START_DATE, NULL AS SOURCE_VALID_END_DATE, ingredient_level.ingredient_concept_id, Standard.TARGET_VALUE_AS_CONCEPT_ID
-from Standard
+select distinct S_S.SOURCE_CODE, Standard.TARGET_CONCEPT_ID, Standard.TARGET_DOMAIN_ID, Standard.VALID_START_DATE, Standard.VALID_END_DATE, Standard.SOURCE_VOCABULARY_ID, Source.TARGET_CONCEPT_ID as SOURCE_TARGET_CONCEPT_ID, Source.SOURCE_VALID_START_DATE as SOURCE_VALID_START_DATE, Source.SOURCE_VALID_END_DATE, ingredient_level.ingredient_concept_id, Standard.TARGET_VALUE_AS_CONCEPT_ID, Source.SOURCE_INVALID_REASON
+from S_S
+left join Standard on Standard.SOURCE_CODE = S_S.SOURCE_CODE
+left join Source on Source.SOURCE_CODE = S_S.SOURCE_CODE 
 left join ingredient_level on ingredient_level.concept_id = Standard.TARGET_CONCEPT_ID
