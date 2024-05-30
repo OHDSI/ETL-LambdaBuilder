4cc811d4b8f21b9eb6499a83fddfa035d7983e36
Anton Ivanov <antoha.ivanov@gmail.com>
2024-05-30 17:29:43 +0300

    Update Batch.sql

--- sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/Premier/Batch.sql
+++ sources/Framework/org.ohdsi.cdm.framework.common3/Core/Transformation/Premier/Batch.sql
@@ -1,6 +1,4 @@
-﻿SELECT DISTINCT medrec_key, medrec_key
+﻿﻿SELECT DISTINCT {0} medrec_key, medrec_key
 FROM {sc}.pat
-JOIN {sc}.patcpt cpt ON PAT.PAT_KEY = cpt.pat_key
-where cpt.cpt_code in ('J1440', 'J9350', 'J9380', 'Q2040', 'Q2041', 'Q2042')
+WHERE medrec_key is not NULL 
 order by 1
-limit 1000000
