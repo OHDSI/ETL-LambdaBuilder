49be42ff63f6b44bf61df10a0257ccbdc095d25b
Anton Ivanov <antoha.ivanov@gmail.com>
2024-07-31 11:16:09 +0300

    massive changes

--- sources/Framework/org.ohdsi.cdm.framework/Common/Enums/Vendor.cs
+++ sources/Framework/org.ohdsi.cdm.framework/Common/Enums/Vendor.cs
@@ -1,6 +1,4 @@
-﻿using org.ohdsi.cdm.framework.common.Utility;
-
-namespace org.ohdsi.cdm.framework.common.Enums
+﻿namespace org.ohdsi.cdm.framework.common.Enums
 {
     public abstract class Vendor
     {
@@ -22,61 +20,9 @@ namespace org.ohdsi.cdm.framework.common.Enums
 
         #region Methods
 
-        public static Vendor CreateVendorInstanceByName(string name)
-        {
-            LoadReferencedAssemblies.DoIfNotLoadedAlready();
-
-            var vendorTypes = AppDomain.CurrentDomain.GetAssemblies()
-                .Where(a => a.ManifestModule.Name == "org.ohdsi.cdm.framework.etl.dll")
-                .SelectMany(assembly => assembly.GetTypes())
-                .Where(t => t.IsSubclassOf(typeof(Vendor))
-                        && !t.IsAbstract);
-
-            var vendorType = vendorTypes.FirstOrDefault(a => a.Name.Contains(name, StringComparison.CurrentCultureIgnoreCase));
-
-            if (vendorType == null)
-            {
-                name = name.ToLower().Replace("v5", "").Replace("full", "");
-
-                vendorType = vendorTypes.FirstOrDefault(a => a.Name.Contains(name, StringComparison.CurrentCultureIgnoreCase));
-            }
-
-            var handle = (Vendor)Activator.CreateInstance(vendorType);
-
-            return handle;
-        }
-
         public override string ToString()
         {
             return this.Name;
-        }
-
-        public string ToStringFull()
-        {
-            var res = string.Join(" - ", new string[]
-               {
-                this.Name,
-                this.Folder,
-                this.Description,
-                this.CdmSource,
-                this.CdmVersion.ToString(),
-                this.PersonIdIndex.ToString(),
-                this.PersonTableName,
-                this.Citation,
-                this.Publication
-               });
-            return res;
-        }
-
-        public override bool Equals(object obj)
-        {
-            var objCast = (Vendor)obj;
-            return this.ToStringFull() == objCast.ToStringFull();
-        }
-
-        public override int GetHashCode()
-        {
-            return this.ToStringFull().GetHashCode();
         }
 
         #endregion
