06125dd6a05beef14a19ad6decde79dd5e2caf30
Valeriy Sedov <navirter@gmail.com>
2024-09-27 18:00:26 +0200

    etlLibraryPath variable is pointless, it should be passed as parameter instead of a hardcoded ""

--- sources/Tests/RunLocal/Program.cs
+++ sources/Tests/RunLocal/Program.cs
@@ -27,8 +27,7 @@ namespace RunLocal
 
             Console.WriteLine($"{Directory.GetCurrentDirectory()}");
 
-            var etlLibraryPath = "";
-            Process(EtlLibrary.CreateVendorInstance(etlLibraryPath, args[0]), int.Parse(args[1]), int.Parse(args[2]), args[3], bool.Parse(args[4]), etlLibraryPath);
+            Process(EtlLibrary.CreateVendorInstance(args[5], args[0]), int.Parse(args[1]), int.Parse(args[2]), args[3], bool.Parse(args[4]), args[5]);
 
             //int[] slicesNum = [24, 40, 48, 96, 192];
 
