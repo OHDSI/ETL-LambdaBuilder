a4c306831a374e86fd9e777978e037bf40eb4664
Anton Ivanov <antoha.ivanov@gmail.com>
2024-11-27 13:37:22 +0300

    Update Batch.sql

--- sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/HealthVerity/Batch.sql
+++ sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/HealthVerity/Batch.sql
@@ -6,6 +6,9 @@ where lower(benefit_type) = 'medical'
 union
 SELECT DISTINCT hvid FROM {sc}.enrollment 
 where lower(benefit_type) = 'pharmacy' or benefit_type is null
+union
+SELECT DISTINCT hvid FROM {sc}.enrollment 
+where hvid in ('00000caf25c3dbcc022d2c1690af3166', 'd885cef572b581672aa0fa27d5f9b9de')
 ) a 
 where hvid != '' and hvid is not null
 order by 1
