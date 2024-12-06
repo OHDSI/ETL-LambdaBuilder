49b6a563cf1f5da6ade61030894c3f348d9c0a8c
Anton Ivanov <antoha.ivanov@gmail.com>
2024-12-06 13:38:54 +0300

    Update Batch.sql

--- sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/HealthVerity/Batch.sql
+++ sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/HealthVerity/Batch.sql
@@ -4,7 +4,8 @@ from
 SELECT distinct e.hvid
 from {sc}.events e
 join {sc}.medical_claims m on e.hvid = m.hvid and m.date_service > dateadd(day, 60, e.event_date)
-where lower(benefit_type) = 'medical' or (lower(benefit_type) = 'pharmacy' or benefit_type is null)
+join {sc}.enrollment enr on e.hvid = enr.hvid
+where lower(enr.benefit_type) = 'medical' or (lower(enr.benefit_type) = 'pharmacy' or enr.benefit_type is null)
 union
 SELECT DISTINCT hvid FROM {sc}.enrollment 
 where lower(benefit_type) = 'medical'
