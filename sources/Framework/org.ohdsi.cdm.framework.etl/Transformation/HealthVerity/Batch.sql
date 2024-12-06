b8d1365acab799dcaea05b4f6ff365ee0c2dcfdf
Anton Ivanov <antoha.ivanov@gmail.com>
2024-12-06 12:41:32 +0300

    Update Batch.sql

--- sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/HealthVerity/Batch.sql
+++ sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/HealthVerity/Batch.sql
@@ -1,8 +1,10 @@
 ﻿SELECT DISTINCT {0} row_number() over (order by hvid) person_id, hvid
 from 
 (
-SELECT DISTINCT hvid FROM {sc}.enrollment 
-where hvid in ('00000caf25c3dbcc022d2c1690af3166', 'd885cef572b581672aa0fa27d5f9b9de')
+SELECT distinct e.hvid
+from {sc}.events e
+join {sc}.medical_claims m on e.hvid = m.hvid and m.date_service > dateadd(day, 60, e.event_date)
+where lower(benefit_type) = 'medical' or (lower(benefit_type) = 'pharmacy' or benefit_type is null)
 union
 SELECT DISTINCT hvid FROM {sc}.enrollment 
 where lower(benefit_type) = 'medical'
