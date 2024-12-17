31cc432ee3e52683d52c864479a4f1062e7957eb
Anton Ivanov <antoha.ivanov@gmail.com>
2024-12-17 12:37:35 +0300

    Update Batch.sql

--- sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/OptumExtended/Batch.sql
+++ sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/OptumExtended/Batch.sql
@@ -1,3 +1,18 @@
-﻿SELECT DISTINCT {0}  patid, patid
-FROM {sc}.MEMBER_CONTINUOUS_ENROLLMENT
-order by 1
+﻿SELECT distinct patid, patid
+FROM {sc}.med_diagnosis
+where diag in
+(
+'798', --	"Sudden death, cause unknown"
+'E978', --	Legal execution
+'P95', --	Fetal death of unspecified cause
+'R95', --	Sudden infant death syndrome
+'R96', --	"Other sudden death, cause unknown"
+'R99' --	Ill-defined and unknown cause of mortality
+)
+union
+SELECT distinct patid, patid
+FROM {sc}.medical_claims
+where DSTATUS IN ('20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '40', '41', '42')
+union
+SELECT distinct patid, patid
+FROM {sc}.DEATH
