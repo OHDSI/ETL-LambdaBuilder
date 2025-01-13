ce96731249d2828e8a6a1d0690b6b1ed79148f9c
Anton Ivanov <antoha.ivanov@gmail.com>
2025-01-13 15:06:37 +0300

    Update Batch.sql

--- sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/HealthVerity/Batch.sql
+++ sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/HealthVerity/Batch.sql
@@ -1,4 +1,4 @@
-﻿SELECT DISTINCT {0} row_number() over (order by hvid) person_id, hvid
+﻿SELECT DISTINCT {0} row_number() over (order by person_source_value) person_id, person_source_value
 from 
 (
 SELECT person_source_value FROM native_hv_cc_quarterly_sample._ch
