975209720d6c1dd0594cd9a9b6a7cd8e2fcc51eb
Anton Ivanov <antoha.ivanov@gmail.com>
2025-01-14 12:51:28 +0300

    Update Batch.sql

--- sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/HealthVerity/Batch.sql
+++ sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/HealthVerity/Batch.sql
@@ -1,3 +1,5 @@
-﻿SELECT person_id, person_source_value 
-FROM native_hv_cc_quarterly_sample._ch
+﻿SELECT distinct c.person_id, c.person_source_value 
+FROM native_hv_cc_quarterly_sample._ch c
+join {sc}.enrollment e on e.hvid = c.person_source_value
+where lower(e.benefit_type) = 'medical' or lower(e.benefit_type) = 'pharmacy' or e.benefit_type is null
 order by 1
