f822df9e3f5dbbc8e9aa185eca7349a052e7fdd7
Anton Ivanov <antoha.ivanov@gmail.com>
2025-05-05 14:58:33 +0300

    Update ObservationTests.R

--- docs/PREMIER/Test Cases/R/ObservationTests.R
+++ docs/PREMIER/Test Cases/R/ObservationTests.R
@@ -69,9 +69,9 @@ createObservationTests <- function () {
                   icd_version = 9);
   expect_observation(person_id                = patient$person_id,
                      visit_occurrence_id      = visit$visit_occurrence_id,
-                     observation_concept_id   = 4206254,
+                     observation_concept_id   = 443423,
                      observation_date         = "2008-05-01",
-                     observation_source_value = "V56.32");
+                     observation_source_value = "E826");
 
   # PATICD_PROC.ICD_CODE ICD10CM;
   patient <- createPatient();
