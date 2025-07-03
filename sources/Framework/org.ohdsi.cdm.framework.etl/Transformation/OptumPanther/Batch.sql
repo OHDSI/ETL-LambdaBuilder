3b1d4e24ec1f93e80ea14c3b5761cb6ee4a76d0c
Anton Ivanov <antoha.ivanov@gmail.com>
2025-07-03 13:31:42 +0300

    Update Batch.sql

--- sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/OptumPanther/Batch.sql
+++ sources/Framework/org.ohdsi.cdm.framework.etl/Transformation/OptumPanther/Batch.sql
@@ -4,16 +4,4 @@
 
 SELECT DISTINCT {0} cast(replace(ptid, 'PT', '') as int) person_id, ptid
 FROM {sc}.patient
-where ptid in
-(
-select distinct ptid from {sc}.alz_assessment
-union
-select distinct ptid from {sc}.alz_biomarker
-union
-select distinct ptid from {sc}.alz_imaging
-union
-select distinct ptid from {sc}.alz_medication
-union
-select distinct ptid from {sc}.alz_problem
-)
 order by 1
