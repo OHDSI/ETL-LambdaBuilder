d2d9f702c4d29e3a5a617041f8efab2787f43779
Anton Ivanov <antoha.ivanov@gmail.com>
2025-08-21 15:05:26 +0300

    Update CreateChunkTable.sql

--- sources/Framework/org.ohdsi.cdm.framework/Common/Common/Databricks/CreateChunkTable.sql
+++ sources/Framework/org.ohdsi.cdm.framework/Common/Common/Databricks/CreateChunkTable.sql
@@ -1,8 +1,8 @@
-﻿CREATE TABLE `{sc}`._chunks (ChunkId int, PartitionId int, PERSON_ID bigint NOT NULL, PERSON_SOURCE_VALUE varchar(50));
+﻿CREATE TABLE {sc}._chunks (ChunkId int, PartitionId int, PERSON_ID bigint NOT NULL, PERSON_SOURCE_VALUE varchar(50));
  
-ALTER TABLE `{sc}`._chunks cluster by (ChunkId, PERSON_ID, PERSON_SOURCE_VALUE);
+ALTER TABLE {sc}._chunks cluster by (ChunkId, PERSON_ID, PERSON_SOURCE_VALUE);
 
-ALTER TABLE `{sc}`._chunks
+ALTER TABLE {sc}._chunks
 set tblproperties (
     'delta.autoOptimize.optimizeWrite' = 'true',
     'delta.autoOptimize.autoCompact' = 'true',
